56f3be1f12504b46827373a3665ac2c9a4231134
Maksymilian Pietrzak <maksymilian.pietrzak@allegro.pl>
2020-04-15 00:32:36 +0200

    folders reorganisation

--- apps/mobile-app/src/app/medy/medy.module.ts
+++ apps/mobile-app/src/app/medy/medy.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { MedyActionButtonComponent } from './components/action-button/action-button';
-import { MedyShowMoreButtonComponent } from './components/show-more-button/show-more-button';
-import { MedyReadOnButtonComponent } from './components/read-on-button/read-on-button';
-import { MedyMoreNewsButtonComponent } from './components/more-news-button/more-news-button';
+import { MedyActionButtonComponent } from './components/button/action/action-button';
+import { MedyShowMoreButtonComponent } from './components/button/show-more/show-more-button';
+import { MedyReadOnButtonComponent } from './components/button/read-on/read-on-button';
+import { MedyMoreNewsButtonComponent } from './components/button/more-news/more-news-button';
 
 import { MedyPreventionStatsComponent } from './components/prevention-stats/prevention-stats';
 import { MedyPreventionPointsCounterComponent } from './components/prevention-points-counter/prevention-points-counter';
