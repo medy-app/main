9135136e921c0f4ad57bd9a7e744ca10f25b1d75
Wojciech Olejnik <w.olejnik@optimusway.io>
2020-04-16 19:08:14 +0200

    Improving after merge

--- apps/admin-dashboard/src/app/app-routing.module.ts
+++ apps/admin-dashboard/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './pages/sign-in/sign-in.module#SignInModule'},
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardModule'},
 ]
 
 @NgModule({
