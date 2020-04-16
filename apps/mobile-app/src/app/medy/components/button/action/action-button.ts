372f97dd835a7bb307013d4aaf3a2b5e1d0d1c99
Maciej Manista <maciej.manista@bynd.com>
2020-04-16 13:48:05 +0100

    feat: actions list - visual rework

--- apps/mobile-app/src/app/medy/components/button/action/action-button.ts
+++ apps/mobile-app/src/app/medy/components/button/action/action-button.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonFab } from '@ionic/angular';
 
 @Component({
   selector: 'medy-action-button',
   templateUrl: 'action-button.html',
-  styleUrls: ['action-button.scss'],
+  styleUrls: ['action-button.scss']
 })
 export class MedyActionButtonComponent {
   constructor() {}
+
+  @ViewChild('fab', { static: true }) fab: IonFab;
+
+  closeActionsList(): void {
+    this.fab.close().catch();
+  }
 }
