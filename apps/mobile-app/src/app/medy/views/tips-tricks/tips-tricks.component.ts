76396948c78e2ea13d4420fbb84369e93e76cde3
Grzegorz Koszuta <ignis.host@gmail.com>
2020-04-19 09:06:05 +0200

    feat (tips tricks) add view

---
+++ apps/mobile-app/src/app/medy/views/tips-tricks/tips-tricks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'medy-tips-tricks',
+  templateUrl: './tips-tricks.component.html',
+  styleUrls: ['./tips-tricks.component.css']
+})
+export class TipsTricksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
