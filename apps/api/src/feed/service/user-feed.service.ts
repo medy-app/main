1956945b5acea3529c9a39bcbf0c46370e5024af
Max <maggzp3@gmail.com>
2020-04-20 12:44:15 +0200

    Maggzp3/moving reusable type defs to libs

--- apps/api/src/feed/service/user-feed.service.ts
+++ apps/api/src/feed/service/user-feed.service.ts
@@ -1,21 +1,20 @@
-import {Injectable} from "@nestjs/common";
-import {InjectModel} from "@nestjs/mongoose";
-import {Model} from "mongoose";
-import {Post} from "../model/db";
-import {FeedDto} from "../model/dto";
-import {Page} from "../../common/model/dto";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Post } from '../model/db';
+import { FeedDto } from '@medy/api-interfaces';
+import { Page } from '../../common/model/dto';
 
 @Injectable()
 export class UserFeedService {
-
-  constructor(@InjectModel("Post") private postModel: Model<Post>) {
-  }
+  constructor(@InjectModel('Post') private postModel: Model<Post>) {}
 
   async getAll(page: number, size: number): Promise<Page<FeedDto>> {
-    const posts = await this.postModel.find()
+    const posts = await this.postModel
+      .find()
       .skip(page * size)
       .limit(size);
-    const contents = posts.map(({_id, title, url, image, created}) => ({
+    const contents = posts.map(({ _id, title, url, image, created }) => ({
       id: _id,
       title,
       url,
@@ -28,6 +27,6 @@ export class UserFeedService {
       totalElements,
       page,
       size
-    }
+    };
   }
 }
